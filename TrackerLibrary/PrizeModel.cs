000373eb0e824387a445893faf0d6a4b8edd41a5
AdamTaylor <adam.f.taylor@hotmail.com>
2021-04-06 14:43:25 +0100

    Started laying the foundations for the Data Connection and wired up the Create Prize form.

--- TrackerLibrary/PrizeModel.cs
+++ TrackerLibrary/PrizeModel.cs
@@ -12,6 +12,11 @@ namespace TrackerLibrary
     public class PrizeModel
     {
         /// <summary>
+        /// The unique identifier for the prize.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
         /// Represents the place number for winning this prize.
         /// </summary>
         public int PlaceNumber { get; set; }
@@ -29,6 +34,29 @@ namespace TrackerLibrary
         /// <summary>
         /// Represents the prize percentage from the prize pool.
         /// </summary>
-        public double PricePercentage { get; set; }
+        public double PrizePercentage { get; set; }
+
+        public PrizeModel()
+        {
+
+        }
+
+        public PrizeModel(string placeName, string placeNumber, string prizeAmount, string prizePercentage)
+        {
+            PlaceName = placeName;
+            
+            int placeNumberValue = 0;
+            int.TryParse(placeNumber, out placeNumberValue);
+            PlaceNumber = placeNumberValue;
+
+            decimal prizeAmountValue = 0;
+            decimal.TryParse(prizeAmount, out prizeAmountValue);
+            PrizeAmount = prizeAmountValue;
+
+            double prizePercentageValue = 0;
+            double.TryParse(prizePercentage, out prizePercentageValue);
+            PrizePercentage = prizePercentageValue;
+
+        }
     }
 }
